4a7af8a172f640af60b5aa37c228fc93a0db09b2
maddhruv <dhruvjainpenny@gmail.com>
2018-11-09 18:38:55 +0530

    made the process easier and smaller

--- bin/good-first-issue.js
+++ bin/good-first-issue.js
@@ -2,11 +2,28 @@
 
 var cli = require('commander')
 
+const log = require('../lib/log')
+const projects = require('../lib/projects')
+const search = require('../lib/search')
+
 cli
   .version('0.4.0', '-v, --version')
   .usage('[options]')
   .description('CLI tool to find good first issues.')
-  .command('node', 'Good First Issues for Node.js')
-  .command('electron', 'Good First Issues for Electron')
-  .command('vscode', 'Good First Issues for VS Code')
+  .arguments('<project>')
+  .action((project) => {
+    search(projects[project].q, (error, issues) => {
+      if(error) throw error
+      
+      // Call the log functionality, output the result to the console.
+      log(issues, projects[project].name, function(error, output) {
+        if(error) throw error
+        // Configure the randomizer for the pool of good-first-issues. This cannot exceed how many entries are actually available from the API.
+        var random = Math.floor(Math.random() * Math.floor(output.length - 1));
+    
+        // Log the issue!
+        console.log(output[random].toString())
+      })
+    })    
+  })
   .parse(process.argv)
