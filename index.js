2a7ee0a2a8ce494f223b5e5e396c9e9d00b98458
Tierney Cyren <hello@bnb.im>
2018-11-17 17:03:40 -0500

    ☑️ Resolve linting errors

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const search = require('./lib/search')
 const projects = require('./lib/projects')
 
 module.exports = async function (project, cb) {
-  return await search(projects[project].q)
+  return search(projects[project].q)
 }
