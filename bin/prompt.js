c64e78aac27c0e4813056a73bfdda39724795d79
emmanuel16com <emmurillocr@gmail.com>
2018-11-17 19:59:57 -0600

    Using the project name instead of the key

--- bin/prompt.js
+++ bin/prompt.js
@@ -3,12 +3,15 @@ const inquirer = require('inquirer')
 const projects = require('../lib/projects')
 
 module.exports = async function () {
+  const projectNames = Object.keys(projects).map(key => {
+    return { value: key, name: projects[key].name || key }
+  })
   let a = await inquirer.prompt([
     {
       type: 'list',
       name: 'project',
       message: 'Choose a project:',
-      choices: Object.keys(projects)
+      choices: projectNames
     }
   ])
   return a.project
