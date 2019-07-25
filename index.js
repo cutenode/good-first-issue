c6f6c0374006eb2d407c550e6b7d57c4d7d82666
Tierney Cyren <hello@bnb.im>
2019-07-25 13:57:42 -0700

    Fix missing changes from migration to enable org/repo search

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const search = require('./lib/search')
 const projects = require('./data/projects.json')
 
 module.exports = async function (project) {
-  return search(projects[project].q)
+  if (project in projects) {
+    return search(projects[project].q)
+  } else {
+    return search(`${project.includes('/') ? 'repo' : 'org'}:${project} state:open label:"good first issue"`)
+  }
 }
