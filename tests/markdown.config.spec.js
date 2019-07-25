73d158b827688cce07d9d7532e90b3bc87b10c8c
Shelley Vohr <shelley.vohr@gmail.com>
2019-07-25 14:17:41 -0700

    test: add specs for data shape and fix README match

--- tests/markdown.config.spec.js
+++ tests/markdown.config.spec.js
@@ -3,14 +3,30 @@ const path = require('path')
 const README_PATH = path.join(__dirname, '..', 'README.md')
 const DATA_FILE_PATH = path.join(__dirname, '..', 'data', 'projects.json')
 
-test('should match README with data/project.json', () => {
+test('should have all the same projects as data/project.json', () => {
+    const projectsFromDatafile = require(DATA_FILE_PATH)
     const projectsFromReadme = getProjectsFromReadme()
-    const projectsFromDatafile = getProjectsFromDatafile()
 
-    expect(projectsFromReadme).toEqual(projectsFromDatafile)
+    const readMeKeys = Object.keys(projectsFromReadme)
+    const dataFileKeys = Object.keys(projectsFromDatafile)
+
+    // ensure both contain all the same top-level projects
+    expect(readMeKeys).toEqual(dataFileKeys)
 })
 
-// below function is influenced by all-contributers-cli, Thanks to @jfmengels and @kentcdodds
+test('data/project.json should have uniform data shape', () => {
+    const projects = require(DATA_FILE_PATH)
+    const dataFileKeys = Object.keys(projects)
+
+    dataFileKeys.forEach(key => {
+        expect(typeof projects[key]).toBe('object')
+        expect(projects[key]).toHaveProperty('name')
+        expect(projects[key]).toHaveProperty('q')
+        expect(projects[key]).toHaveProperty('description')
+    })
+})
+
+// The below function is influenced by all-contributers-cli, Thanks to @jfmengels and @kentcdodds
 // https://github.com/jfmengels/all-contributors-cli/blob/master/src/generate/index.js
 function getProjectsFromReadme() {
     const fileContent = fs.readFileSync(README_PATH, "UTF-8")
@@ -35,22 +51,13 @@ function getProjectsFromReadme() {
     const regex = /\d+\.\|(.*)\|`(.*)`\|/gm;
     var match = regex.exec(table)
 
-    var projects = {};
+    const projects = {};
     while (match != null) {
         let project = match[2]
         let name = match[1]
-        projects[project] = { "name": name }
+        projects[project] = { name }
 
         match = regex.exec(table)
-    }
-
-    return projects
-}
-
-function getProjectsFromDatafile() {
-    let projects = require(DATA_FILE_PATH)
-    for (project in projects) {
-        delete projects[project]["q"]
     }
 
     return projects
