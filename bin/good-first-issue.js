69e6cda894cec4de8ed0bfa403a886eec53c13bc
dhuang612 <dhuang@madisoncollege.edu>
2019-09-04 16:17:01 -0400

    added in Kleur

--- bin/good-first-issue.js
+++ bin/good-first-issue.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const cli = require('commander')
-const chalk = require('chalk')
+const { yellow, white } = require('kleur')
 const opn = require('open')
 const gfi = require('libgfi')
 
@@ -10,7 +10,8 @@ const log = require('../lib/log')
 const prompt = require('../lib/prompt')
 const projects = require('../data/projects.json')
 
-const options = { // options for libgfi
+const options = {
+  // options for libgfi
   projects
 }
 
@@ -33,13 +34,24 @@ cli
 
       if (issues.length === 0) {
         process.exitCode = 0
-        return console.log(chalk.yellow(`\nNo Good First Issues were found for the GitHub organization, repo, or project ${chalk.white(input)}.\n`))
+        return console.log(
+          yellow(
+            `\nNo Good First Issues were found for the GitHub organization, repo, or project ${white(
+              input
+            )}.\n`
+          )
+        )
       }
 
-      const key = cmd.first ? 0 : Math.floor(Math.random() * Math.floor(issues.length - 1))
+      const key = cmd.first
+        ? 0
+        : Math.floor(Math.random() * Math.floor(issues.length - 1))
 
       // Call the log functionality, output the result to the console.
-      const output = await log(issues[key], (input in projects) ? projects[input].name : project)
+      const output = await log(
+        issues[key],
+        input in projects ? projects[input].name : project
+      )
 
       // Log the issue!
       console.log(output.toString())
